99efd253f4cf0fde85eff87f52f2ece29d94d69f
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2014-07-06 16:36:07 -0500

    Registered Models with admin

--- ourapp/admin.py
+++ ourapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from ourapp.models import Restaurant, Customer, Cuisine, GenericDish, Dish, RestaurantReview, DishReview
+
+admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Cuisine)
+admin.site.register(GenericDish)
+admin.site.register(Dish)
+admin.site.register(RestaurantReview)
+admin.site.register(DishReview)
 
 # Register your models here.
