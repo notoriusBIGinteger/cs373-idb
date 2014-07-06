c456261714b1a2ab24ecf9775ab23e42899f0648
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2014-07-06 18:03:57 -0500

    Updated Models as a group of 3

--- ourapp/models.py
+++ ourapp/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+class Cuisine(models.Model) : 
+	name = models.CharField('Name', max_length=30)
+	
+	def __str__(self) : 
+		return str(self.name)	
+
 class Restaurant(models.Model) :
 	name = models.CharField('Name', max_length=30)
 	reservation_required = models.BooleanField('Reservations Required')
 	reservation_avail = models.BooleanField('Reservations Available')
 	has_waiter =models.BooleanField('Waiter Service')
-	phone_number = models.CharField('Phone Number', max_length=50)
+	phone_number = models.CharField('Phone Number', max_length=10)
 	description = models.CharField('Description', max_length=200)
 	zip_code = models.CharField('Zip Code', max_length=5)
 	address = models.CharField('Address', max_length=50)
@@ -14,6 +20,7 @@ class Restaurant(models.Model) :
 	pet_friendly = models.BooleanField('Pet Friendly')
 	cost = models.IntegerField('Cost', max_length=1)
 	website = models.URLField('Website')
+        cuisine = models.ForeignKey(Cuisine)
 	
 	def __str__(self) : 
 		return self.name
@@ -22,18 +29,13 @@ class Restaurant(models.Model) :
 		ordering = ["name"]
 
 class Customer(models.Model) : 
-	name = models.CharField('Name', max_length=15)
-	recent_check_in = models.CharField('Last Check-In', max_length=30)
-	dietary_preference = models.CharField('Diet', max_length=60)
+	name = models.CharField('Name', max_length=35)
+	#recent_check_in = models.CharField('Last Check-In', max_length=30)
+	#dietary_preference = models.CharField('Diet', max_length=60)
 	
 	def __str__(self) : 
 		return self.name
 		
-class Cuisine(models.Model) : 
-	name = models.CharField('Name', max_length=30)
-	
-	def __str__(self) : 
-		return str(self.rating)	
 				
 class GenericDish(models.Model) : 
 	name = models.CharField('Name', max_length=30)
@@ -43,15 +45,17 @@ class GenericDish(models.Model) :
 		return self.name	
 				
 class Dish(models.Model) : 
-	name = models.CharField('Name', max_length=20)
+	name = models.CharField('Name', max_length=40)
 	rating = models.IntegerField('Avg. Rating', max_length=1)
+        num_ratings = models.IntegerField('Number of Ratings')
+        sum_of_ratings = models.IntegerField() # customers shouldn't see this.
 	vegetarian = models.BooleanField('Vegetarian')
 	vegan = models.BooleanField('Vegan')
 	kosher = models.BooleanField('Kosher')
 	halal = models.BooleanField('Halal')
 	nut_allergy = models.BooleanField('Contains Nuts')
 	#image = models.ImageField()
-	price = models.IntegerField('Cost', max_length=1)
+	cost = models.IntegerField('Cost', max_length=1)
 	restaurant = models.ForeignKey(Restaurant)
 	generic_dish = models.ForeignKey(GenericDish)
 	
