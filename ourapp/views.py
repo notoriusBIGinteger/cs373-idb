b083e9e020117904135a154f0bb41a2844628182
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2014-07-06 19:03:38 -0500

    got initial/bad solution for url mapping

--- ourapp/views.py
+++ ourapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def dish(request, dishID):
+  responseString = "hi you are viewing dishID: " + str(dishID)
+  return HttpResponse(responseString)
+
+def restaurant(request, restaurantID):
+  responseString = "hi you are viewing restaurantID: " + str(restaurantID)
+  return HttpResponse(responseString)
+
+def customer(request, customerID):
+  responseString = "hi you are viewing customerID: " + str(customerID)
+  return HttpResponse(responseString)
