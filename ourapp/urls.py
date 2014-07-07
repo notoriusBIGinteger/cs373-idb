94ced0ed1c8ebaaf0f2c7e071696b0b3322d0bc6
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2014-07-06 19:10:49 -0500

    forgot to add file to last commit

---
+++ ourapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from ourapp import views
+
+urlpatterns = patterns('',
+url(r'^dish/(?P<dishID>\d+)/$', views.dish, name='dish'),
+url(r'^restaurant/(?P<restaurantID>\d+)/$', views.restaurant, name='restaurant'),
+url(r'^customer/(?P<customerID>\d+)/$', views.customer, name='customer'),
+)
