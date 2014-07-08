70edab8c3d33e813353b680dc97ba9d508d666b2
Kevin Wheeler <kevinwheeler@users.noreply.github.com>
2014-07-08 18:17:52 -0500

    Correct stubs being rendered based on captured ID. Close #18

--- ourapp/urls.py
+++ ourapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 url(r'^dish/(?P<dishID>\d+)/$', views.dish, name='dish'),
 url(r'^restaurant/(?P<restaurantID>\d+)/$', views.restaurant, name='restaurant'),
 url(r'^customer/(?P<customerID>\d+)/$', views.customer, name='customer'),
+url(r'^index/$', views.index, name='index'),
 )
--- ourapp/views.py
+++ ourapp/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 # Create your views here.
 def customer(request, customerID):
+  customerIDInt = int(customerID) #make int so 1, 01, or 001, all work.
   context = {}
-  return render(request, 'ourapp/customer.html', context)
+  if customerIDInt == 1:
+    return render(request, 'ourapp/customer1.html', context)
+  elif customerIDInt == 2:
+    return render(request, 'ourapp/customer2.html', context)
+  else:
+    raise Http404
 
 def dish(request, dishID):
+  dishIDInt = int(dishID) #make int so 1, 01, or 001, all work.
   context = {}
-  return render(request, 'ourapp/dish.html', context)
+  if dishIDInt == 1:
+    return render(request, 'ourapp/dish1.html', context)
+  elif dishIDInt == 2:
+    return render(request, 'ourapp/dish2.html', context)
+  else:
+    raise Http404
 
 def restaurant(request, restaurantID):
+  restaurantIDInt = int(restaurantID) #make int so 1, 01, or 001, all work.
   context = {}
-  return render(request, 'ourapp/restaurant.html', context)
+  if restaurantIDInt == 1:
+    return render(request, 'ourapp/restaurant1.html', context)
+  elif restaurantIDInt == 2:
+    return render(request, 'ourapp/restaurant2.html', context)
+  else:
+    raise Http404
+
+def index(request):
+  context = {}
+  return render(request, 'ourapp/index.html', context)
