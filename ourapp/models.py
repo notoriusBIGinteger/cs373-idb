dfa70bff401c4aebb2bf655e97817996b4bfffe8
Alex Landaverde <tigresalex11@gmail.com>
2014-07-08 19:13:42 -0500

    added unit testing and some get_all methods within models.

--- ourapp/models.py
+++ ourapp/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-class Cuisine(models.Model) : 
+class Cuisine(models.Model) :
 	name = models.CharField('Name', max_length=30)
-	
-	def __str__(self) : 
-		return str(self.name)	
+
+	def __str__(self) :
+		return str(self.name)
+
+	def set_name(self, name):
+	    self.name = name
+
+	def get_all(self):
+	    return {"name":self.name}
 
 class Restaurant(models.Model) :
 	name = models.CharField('Name', max_length=30)
@@ -21,30 +27,48 @@ class Restaurant(models.Model) :
 	cost = models.IntegerField('Cost', max_length=1)
 	website = models.URLField('Website')
 	cuisine = models.ForeignKey(Cuisine)
-	
-	def __str__(self) : 
+
+	def __str__(self) :
 		return self.name
-	
+
 	class Meta:
 		ordering = ["name"]
 
-class Customer(models.Model) : 
+	def get_all(self):
+	    return {"name": self.name,
+	            "reservation_required": self.reservation_required,
+	            "reservation_avail": self.reservation_avail ,
+	            "has_waiter": self.has_waiter,
+	            "phone_number": self.phone_number,
+	            "description": self.description,
+	            "zip_code": self.zip_code,
+	            "address": self.address,
+	            "delivery":self.delivery,
+	            "take_out":self.take_out,
+	            "pet_friendly":self.pet_friendly,
+	            "cost":self.cost,
+	            "website":self.website}
+
+class Customer(models.Model) :
 	name = models.CharField('Name', max_length=35)
 	#recent_check_in = models.CharField('Last Check-In', max_length=30)
 	#dietary_preference = models.CharField('Diet', max_length=60)
-	
-	def __str__(self) : 
+
+	def __str__(self) :
 		return self.name
-		
-				
-class GenericDish(models.Model) : 
+	def get_all(self):
+	    return {"name":self.name}
+
+class GenericDish(models.Model) :
 	name = models.CharField('Name', max_length=30)
 	cuisine = models.ForeignKey(Cuisine)
-	
-	def __str__(self) : 
-		return self.name	
-				
-class Dish(models.Model) : 
+
+	def __str__(self) :
+		return self.name
+	def get_all(self):
+		return {"name" : self.name}
+
+class Dish(models.Model) :
 	name = models.CharField('Name', max_length=40)
 	rating = models.IntegerField('Avg. Rating', max_length=1)
 	num_ratings = models.IntegerField('Number of Ratings')
@@ -58,24 +82,44 @@ class Dish(models.Model) :
 	cost = models.IntegerField('Cost', max_length=1)
 	restaurant = models.ForeignKey(Restaurant)
 	generic_dish = models.ForeignKey(GenericDish)
-	
-	def __str__(self) : 
+
+	def __str__(self) :
 		return self.name
 
-class RestaurantReview(models.Model) : 
+	def get_all(self):
+		return {"name": self.name,
+				"rating": self.rating,
+				"num_ratings":self.num_ratings,
+				"sum_of_ratings":self.sum_of_ratings,
+				"vegetarian":self.vegetarian,
+				"vegan":self.vegan,
+				"kosher":self.kosher,
+				"halal":self.halal,
+				"nut_allergy":self.nut_allergy,
+				"cost":self.cost}
+
+class RestaurantReview(models.Model) :
 	rating = models.IntegerField('Rating', max_length=1)
 	review_comment = models.CharField('Comment', max_length=250)
 	customer = models.ForeignKey(Customer)
 	restaurant = models.ForeignKey(Restaurant)
-	
-	def __str__(self) : 
+
+	def __str__(self) :
 		return str(self.rating)
 
-class DishReview(models.Model) : 
+	def get_all(self):
+		return {"rating": self.rating,
+				"review_comment":self.review_comment}
+
+class DishReview(models.Model) :
 	rating = models.IntegerField('Rating', max_length=1)
 	review_comment = models.CharField('Comment', max_length=250)
 	customer = models.ForeignKey(Customer)
 	dish = models.ForeignKey(Dish)
-	
-	def __str__(self) : 
-		return str(self.rating)		
+
+	def __str__(self) :
+		return str(self.rating)
+
+	def get_all(self):
+		return {"rating": self.rating,
+				"review_comment":self.review_comment}
--- ourapp/tests.py
+++ ourapp/tests.py
@@ -1,3 +1,262 @@
 from django.test import TestCase
+from ourapp.models import Cuisine, Restaurant, Customer, GenericDish, Dish, RestaurantReview, DishReview
 
-# Create your tests here.
+
+class CuisineMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        c = Cuisine(name = "Italian")
+        self.assertEqual(c.get_all(), {"name": "Italian"})
+
+    def test_get_all_2(self):
+        c = Cuisine(name = "Mexican")
+        self.assertEqual(c.get_all(), {"name": "Mexican"})
+
+    def test_get_all_3(self):
+        c = Cuisine(name = "American")
+        self.assertEqual(c.get_all(), {"name": "American"})
+
+    # ---------
+    # set_name
+    # ---------
+    def test_set_name_1(self):
+        c = Cuisine(name = "Italian")
+        self.assertEqual(c.name, "Italian")
+        c.set_name("American")
+        self.assertEqual(c.name, "American")
+
+    def test_set_name_2(self):
+        c = Cuisine(name = "Italian")
+        self.assertEqual(c.name, "Italian")
+        c.set_name("Mexican")
+        self.assertEqual(c.name, "Mexican")
+
+    def test_set_name_3(self):
+        c = Cuisine(name = "Italian")
+        self.assertEqual(c.name, "Italian")
+        c.set_name("Asian")
+        self.assertEqual(c.name, "Asian")
+
+class RestaurantMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        r = Restaurant(name="Kerbey Lane",
+                       reservation_required = False,
+                       reservation_avail = False,
+                       has_waiter = True,
+                       phone_number = "5125555555",
+                       description = "Diner Food",
+                       zip_code = "78741",
+                       address = "Austin",
+                       delivery = False,
+                       take_out = False,
+                       pet_friendly = True,
+                       cost = 2,
+                       website = "http://fake.com")
+        self.assertEqual(r.get_all(), {"name": "Kerbey Lane",
+	                                   "reservation_required": False,
+	                                   "reservation_avail": False,
+	                                   "has_waiter": True,
+	                                   "phone_number":"5125555555" ,
+	                                   "description": "Diner Food",
+	                                   "zip_code": "78741",
+	                                   "address": "Austin",
+	                                   "delivery":False,
+	                                   "take_out":False,
+	                                   "pet_friendly":True,
+	                                   "cost":2,
+	                                   "website":"http://fake.com"})
+    def test_get_all_2(self):
+        r = Restaurant(name="Magnolia Cafe",
+                       reservation_required = False,
+                       reservation_avail = False,
+                       has_waiter = True,
+                       phone_number = "5125555555",
+                       description = "Diner",
+                       zip_code = "78741",
+                       address = "Austin",
+                       delivery = False,
+                       take_out = True,
+                       pet_friendly = True,
+                       cost = 2,
+                       website = "http://fakewebsite.com")
+        self.assertEqual(r.get_all(), {"name": "Magnolia Cafe",
+	                                   "reservation_required": False,
+	                                   "reservation_avail": False,
+	                                   "has_waiter": True,
+	                                   "phone_number":"5125555555" ,
+	                                   "description": "Diner",
+	                                   "zip_code": "78741",
+	                                   "address": "Austin",
+	                                   "delivery":False,
+	                                   "take_out":True,
+	                                   "pet_friendly":True,
+	                                   "cost":2,
+	                                   "website":"http://fakewebsite.com"})
+
+    def test_get_all_3(self):
+        r = Restaurant(name="Thai Spice",
+                       reservation_required = False,
+                       reservation_avail = True,
+                       has_waiter = True,
+                       phone_number = "5125555555",
+                       description = "Asian",
+                       zip_code = "78741",
+                       address = "Austin",
+                       delivery = True,
+                       take_out = True,
+                       pet_friendly = False,
+                       cost = 2,
+                       website = "http://2spicy4U.com")
+        self.assertEqual(r.get_all(), {"name": "Thai Spice",
+	                                   "reservation_required": False,
+	                                   "reservation_avail": True,
+	                                   "has_waiter": True,
+	                                   "phone_number":"5125555555" ,
+	                                   "description": "Asian",
+	                                   "zip_code": "78741",
+	                                   "address": "Austin",
+	                                   "delivery":True,
+	                                   "take_out":True,
+	                                   "pet_friendly":False,
+	                                   "cost":2,
+	                                   "website":"http://2spicy4U.com"})
+
+class CustomerMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        c = Customer(name = "Bobby")
+        self.assertEqual(c.get_all(), {"name": "Bobby"})
+
+    def test_get_all_2(self):
+        c = Customer(name = "Sam")
+        self.assertEqual(c.get_all(), {"name": "Sam"})
+
+    def test_get_all_3(self):
+        c = Customer(name = "Dean")
+        self.assertEqual(c.get_all(), {"name": "Dean"})
+
+class GenericDishMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        gd = GenericDish(name = "Hamburger")
+        self.assertEqual(gd.get_all(), {"name":"Hamburger"})
+
+    def test_get_all_2(self):
+        gd = GenericDish(name = "Pizza")
+        self.assertEqual(gd.get_all(), {"name":"Pizza"})
+
+    def test_get_all_3(self):
+        gd = GenericDish(name = "Sushi")
+        self.assertEqual(gd.get_all(), {"name":"Sushi"})
+
+class DishMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        d = Dish(name = "BLT Sandwhich",
+                 rating = 2,
+                 num_ratings = 3,
+                 sum_of_ratings = 5,
+                 vegetarian = False,
+                 vegan = False,
+                 kosher = False,
+                 halal = False,
+                 nut_allergy = True,
+                 cost = 4)
+        self.assertEqual(d.get_all(),{"name":"BLT Sandwhich",
+                                      "rating":2,
+                                      "num_ratings":3,
+                                      "sum_of_ratings":5,
+                                      "vegetarian":False,
+                                      "vegan":False,
+                                      "kosher":False,
+                                      "halal":False,
+                                      "nut_allergy":True,
+                                      "cost" : 4})
+
+    def test_get_all_2(self):
+        d = Dish(name = "Beef Taco",
+                 rating = 4,
+                 num_ratings = 10,
+                 sum_of_ratings = 6,
+                 vegetarian = False,
+                 vegan = False,
+                 kosher = True,
+                 halal = False,
+                 nut_allergy = False,
+                 cost = 2)
+        self.assertEqual(d.get_all(),{"name":"Beef Taco",
+                                      "rating":4,
+                                      "num_ratings":10,
+                                      "sum_of_ratings":6,
+                                      "vegetarian":False,
+                                      "vegan":False,
+                                      "kosher":True,
+                                      "halal":False,
+                                      "nut_allergy":False,
+                                      "cost" : 2})
+
+    def test_get_all_3(self):
+        d = Dish(name = "Pancake",
+                 rating = 4,
+                 num_ratings = 11,
+                 sum_of_ratings = 8,
+                 vegetarian = True,
+                 vegan = True,
+                 kosher = True,
+                 halal = True,
+                 nut_allergy = False,
+                 cost = 3)
+        self.assertEqual(d.get_all(),{"name":"Pancake",
+                                      "rating":4,
+                                      "num_ratings":11,
+                                      "sum_of_ratings":8,
+                                      "vegetarian":True,
+                                      "vegan":True,
+                                      "kosher":True,
+                                      "halal":True,
+                                      "nut_allergy":False,
+                                      "cost" : 3})
+
+class RestaurantReviewMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        rr = RestaurantReview(rating = 3, review_comment = "Okay")
+        self.assertEqual(rr.get_all(), {"rating":3, "review_comment":"Okay"})
+
+    def test_get_all_2(self):
+        rr = RestaurantReview(rating = 4, review_comment = "")
+        self.assertEqual(rr.get_all(), {"rating":4, "review_comment":""})
+
+    def test_get_all_3(self):
+        rr = RestaurantReview(rating = 1, review_comment = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium.")
+        self.assertEqual(rr.get_all(), {"rating":1, "review_comment":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium."})
+
+class DishReviewMethodTests(TestCase):
+    # --------
+    # get_all
+    # --------
+    def test_get_all_1(self):
+        dr = DishReview(rating = 3, review_comment = "Okay")
+        self.assertEqual(dr.get_all(), {"rating":3, "review_comment":"Okay"})
+
+    def test_get_all_2(self):
+        dr = DishReview(rating = 1, review_comment = "")
+        self.assertEqual(dr.get_all(), {"rating":1, "review_comment":""})
+
+    def test_get_all_3(self):
+        dr = DishReview(rating = 5, review_comment = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium.")
+        self.assertEqual(dr.get_all(), {"rating":5, "review_comment":"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium."})
+
